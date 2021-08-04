6696136ef4ccda11e91da54fc7bc0235eaa83e0a
happyntec <tec@happyn.cc>
2021-08-03 20:18:53 -0700

    HPYSERVER-13

--- src/edge.c
+++ src/edge.c
@@ -56,6 +56,53 @@ int edge_init_routes (n2n_edge_t *eee, n2n_route_t *routes, uint16_t num_routes)
 
 /* ***************************************************** */
 
+int inet_pton_ex(int af, const char *src, void *dst)
+{
+  struct sockaddr_storage ss;
+  int size = sizeof(ss);
+  char src_copy[INET6_ADDRSTRLEN+1];
+
+  ZeroMemory(&ss, sizeof(ss));
+  /* stupid non-const API */
+  strncpy (src_copy, src, INET6_ADDRSTRLEN+1);
+  src_copy[INET6_ADDRSTRLEN] = 0;
+
+  if (WSAStringToAddress(src_copy, af, NULL, (struct sockaddr *)&ss, &size) == 0) {
+    switch(af) {
+      case AF_INET:
+    *(struct in_addr *)dst = ((struct sockaddr_in *)&ss)->sin_addr;
+    return 1;
+      case AF_INET6:
+    *(struct in6_addr *)dst = ((struct sockaddr_in6 *)&ss)->sin6_addr;
+    return 1;
+    }
+  }
+  return 0;
+}
+
+const char *inet_ntop_ex(int af, const void *src, char *dst, socklen_t size)
+{
+  struct sockaddr_storage ss;
+  unsigned long s = size;
+
+  ZeroMemory(&ss, sizeof(ss));
+  ss.ss_family = af;
+
+  switch(af) {
+    case AF_INET:
+      ((struct sockaddr_in *)&ss)->sin_addr = *(struct in_addr *)src;
+      break;
+    case AF_INET6:
+      ((struct sockaddr_in6 *)&ss)->sin6_addr = *(struct in6_addr *)src;
+      break;
+    default:
+      return NULL;
+  }
+  /* cannot direclty use &size because of strict aliasing rules */
+  return (WSAAddressToString((struct sockaddr *)&ss, sizeof(ss), NULL, dst, &s) == 0)?
+          dst : NULL;
+}
+
 /** Find the address and IP mode for the tuntap device.
  *
  *    s is of the form:
@@ -116,7 +163,7 @@ static int scan_address (char * ip_addr, size_t addr_size,
         // now, handle the sub-network address
         sscanf(end + 1, "%u", &bitlen);
         bitlen = htobe32(bitlen2mask(bitlen));
-        inet_ntop(AF_INET, &bitlen, netmask, netmask_size);
+        inet_ntop_ex(AF_INET, &bitlen, netmask, netmask_size);
     }
 
     return retval;
